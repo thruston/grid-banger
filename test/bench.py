21f647be709278207ab476e882e0435419c88a50
Toby Thurston <thurston@eml.cc>
2016-03-17 12:18:24 +0000

    Added bench mark test

---
+++ test/bench.py
@@ -0,0 +1,13 @@
+import timeit
+
+n = 10
+t = timeit.timeit("osgb.ll_to_grid(52 + 2 * random.random(), 0-3*random.random())",
+        setup = "import random; import osgb",
+        number=n*1000)
+s = timeit.timeit("osgb.grid_to_ll(random.randrange(400000,500000), random.randrange(200000,500000))",
+#s = timeit.timeit("osgb.grid_to_ll(393154.801, 177900.605)",
+        setup = "import random; import osgb",
+        number=n*1000)
+
+print("ll_to_grid: {:5d}/s {:.3g} ms per call".format(int(1000*n/t), t/n))
+print("grid_to_ll: {:5d}/s {:.3g} ms per call".format(int(1000*n/s), s/n))
