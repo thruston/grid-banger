4f492677533dcc74168c3832cc65634f00bd5f1a
thruston <663059+thruston@users.noreply.github.com>
2021-08-23 16:11:59 +0100

    add doctests

--- scripts/whatmaps.py
+++ scripts/whatmaps.py
@@ -14,47 +14,48 @@ import collections
 import gpxpy
 import osgb
 
-parser = argparse.ArgumentParser(description="Find all the maps needed to cover a route or track in the GPX file")
-parser.add_argument("gpxfile", help="The name of your GPX file")
-parser.add_argument("--series", default="A",
-                    help="Which map series? A: Landranger, B: Explorer, C: One-inch, ...")
-parser.add_argument("--name", action="store_true", help="Show name of the series")
-parser.add_argument("--title", action="store_true", help="Show the title of the map")
-parser.add_argument("--coverage", action="store_true",
-                    help="Show how many of the points are covered by each map")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Find all the maps needed to cover a route or track in the GPX file")
+    parser.add_argument("gpxfile", help="The name of your GPX file")
+    parser.add_argument("--series", default="A",
+                        help="Which map series? A: Landranger, B: Explorer, C: One-inch, ...")
+    parser.add_argument("--name", action="store_true", help="Show name of the series")
+    parser.add_argument("--title", action="store_true", help="Show the title of the map")
+    parser.add_argument("--coverage", action="store_true",
+                        help="Show how many of the points are covered by each map")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-with open(args.gpxfile) as g:
-    gpx = gpxpy.parse(g)
+    with open(args.gpxfile) as g:
+        gpx = gpxpy.parse(g)
 
-points = list()
-for track in gpx.tracks:
-    for segment in track.segments:
-        for point in segment.points:
-            points.append(point)
+    points = list()
+    for track in gpx.tracks:
+        for segment in track.segments:
+            for point in segment.points:
+                points.append(point)
 
-for point in gpx.waypoints:
-    points.append(point)
-
-for route in gpx.routes:
-    for point in route.points:
+    for point in gpx.waypoints:
         points.append(point)
 
-maps = list()
-for p in points:
-    maps.extend(osgb.sheet_keys(*osgb.ll_to_grid(p.latitude, p.longitude), series=args.series))
+    for route in gpx.routes:
+        for point in route.points:
+            points.append(point)
 
-c = collections.Counter(maps)
-for map_key in sorted(c):
-    out = list()
-    if args.name:
-        k, sheet = map_key.split(':')
-        out.append('{} {}'.format(osgb.name_for_map_series[k], sheet))
-    else:
-        out.append(map_key)
-    if args.title:
-        out.append('"' + osgb.map_locker[map_key].title + '"')
-    if args.coverage:
-        out.append("({}%)".format(int(100*c[map_key]/len(points))))
-    print(' '.join(out))
+    maps = list()
+    for p in points:
+        maps.extend(osgb.sheet_keys(*osgb.ll_to_grid(p.latitude, p.longitude), series=args.series))
+
+    c = collections.Counter(maps)
+    for map_key in sorted(c):
+        out = list()
+        if args.name:
+            k, sheet = map_key.split(':')
+            out.append('{} {}'.format(osgb.name_for_map_series[k], sheet))
+        else:
+            out.append(map_key)
+        if args.title:
+            out.append('"' + osgb.map_locker[map_key].title + '"')
+        if args.coverage:
+            out.append("({}%)".format(int(100*c[map_key]/len(points))))
+        print(' '.join(out))
